2feef44af3f94bf5ecaa2683c732c00155ee3801
ajj10 <37146798+ajj10@users.noreply.github.com>
2021-10-06 09:57:55 +0200

    cleaning data
    Búinn að eyða út öllum R sem koma ekki fyrir í t strengjunum okkar og eyða öllum r sem geta ekki verið partur af strengnum s

--- Code/decoder.py
+++ Code/decoder.py
@@ -3,7 +3,7 @@
 #       populates list and variables accordingly 
 #######################################################
 
-f = open("tests/test02.swe", "r")
+f = open("tests/test04.swe", "r")
 k = int(f.readline())
 s = f.readline().strip()
 t_strings = []
@@ -11,8 +11,10 @@ for x in range(0, k):
     t_strings.append(f.readline().strip())
 myline = f.readline().strip()
 dict_of_R = {}
+unused_Rs = []
 while(myline):
     tmp_line = myline.split(":")
+    unused_Rs.append(tmp_line[0])
     tmp_list_items = tmp_line[1].split(",")
     tmp_list = []
     for x in tmp_list_items:
@@ -36,3 +38,29 @@ f.close()
 #    exists and returns is if it does otherwise NO
 #######################################################
 
+# Lets start by cutting out all r elements in each R by checking if they alone are a substring of s
+# thus eliminating all r's that could never form a substring of s
+# and also get rid of all R that are unused (never appear in any of our t strings)
+for x in t_strings:
+    tmp_len = len(x)
+    for i in range(0, tmp_len):
+        if(x[i].isupper()):
+            if x[i] in unused_Rs:
+                unused_Rs.remove(x[i])
+            tmp_dict_list = dict_of_R[x[i]]
+            tmp_dict_list_len = len(tmp_dict_list)
+            tmp_index_list = []
+            for j in range(0, tmp_dict_list_len):
+                if tmp_dict_list[j] not in s:
+                    tmp_index_list.append(j)
+            tmp_index_list.sort(reverse=True)
+            for m in tmp_index_list:
+                tmp_dict_list.pop(m)
+            dict_of_R[x[i]] = tmp_dict_list
+
+for x in unused_Rs:
+    del dict_of_R[x]
+
+dictionary_items = dict_of_R.items()
+for item in dictionary_items:
+    print(item)
